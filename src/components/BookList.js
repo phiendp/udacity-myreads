da3a17c3623c06ead5b8bef750a7f84a160f0f68
Phien Pham <phien@triip.me>
2017-09-02 10:38:24 +0700

    Render BookList component with 3 statuses
    - Currently Reading, Want to Read, Read

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
+import BookShelf from './bookshelf'
 
 class BookList extends Component {
   render() {
+    const { books } = this.props
     return (
       <div className="list-books">
         <div className="list-books-title">
           <h1>MyReads</h1>
         </div>
+        
+        <div className="list-books-content">
+          <div className="list-books-content">
+            <div>
+              <BookShelf
+                name="Currently Reading"
+                books={ books }
+                // updateBookShelf={ updateBookShelf }
+              />
+              <BookShelf
+                name="Want to Read"
+                books={ books }
+                // updateBookShelf={ updateBookShelf }
+              />
+              <BookShelf
+                name="Read"
+                books={ books }
+                // updateBookShelf={ updateBookShelf }
+              />
+            </div>
+          </div>
+        </div>          
       </div>
     );
   }
