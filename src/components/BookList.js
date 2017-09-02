cc7e01a238e85691d6951012a0921cda903e70b8
Phien Pham <phien@triip.me>
2017-09-02 09:11:15 +0700

    Render initial BookList component

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,5 +2,14 @@ import React, { Component } from 'react';
 
 class BookList extends Component {
   render() {
+    return (
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+      </div>
+    );
   }
 }
+
+export default BookList
