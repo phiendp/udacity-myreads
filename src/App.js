469372521be036e8379d76e157b6562b5f4379b4
Phien Pham <phien@triip.me>
2017-09-02 10:37:45 +0700

    Get all books from BooksAPI and send them as props to BookList component

--- src/App.js
+++ src/App.js
@@ -5,20 +5,21 @@ import BookList from './components/booklist';
 
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: true
+  constructor() {
+    super();
+    this.state = BooksAPI.getAll().then((books) => {
+      this.setState({ books: books })
+    })
   }
 
   render() {
+    const { books } = this.state;
+    if (!books)
+      return null;
+
     return(
       <div className="app">
-        <BookList />
+        <BookList books={ books }/>
       </div>
     )
   }
