6b96eef3f16e2aa3177447850303277ec65d40ef
Phien Pham <phien@triip.me>
2017-09-02 10:39:11 +0700

    Render BookShelf component
    - Each bookshelf contains a number of books with title, cover image, authors, current shelf's status

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,10 +1,34 @@
-import React, { Component } from 'react';
-import Link
+import React from 'react';
+import Book from './book'
 
 
-class BookShelve extend Component {
-  render() {
-  }
+function BookShelf({ name, books}) {
+  return(
+    <div className="bookshelf">
+      <h2 className="bookshelf-title"> {name} </h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid"> 
+          {
+            books.sort((a,b) => {
+              return a.title > b.title
+            }).map((book) => 
+              <li key={ book.id }>
+                <Book 
+                  id={ book.id }
+                  authors={ book.authors }
+                  title={ book.title }
+                  imageLinks={ book.imageLinks }
+                  shelf={ book.shelf }
+                  // updateShelf={ updateShelf }
+                />
+              </li>
+            )
+          }
+        </ol>
+      </div>
+
+    </div>
+  );
 }
 
-export default BookShelve
+export default BookShelf
