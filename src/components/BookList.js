78a04b6cb35d2be446cecda30e5dbaecc026a1a0
Phien Pham <phien@triip.me>
2017-09-05 11:33:19 +0700

    Perform update the book's self when user switches the selection on status

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,39 +1,52 @@
 import React, { Component } from 'react';
-import BookShelf from './BookShelf'
+import { Link } from 'react-router-dom';
+
+import Bookshelf from './BookShelf';
 
 class BookList extends Component {
-  render() {
-    const { books } = this.props
-    return (
+
+  _filterBooks = (shelf) => {
+    const { books } = this.props;
+    return books.filter((book) => book.shelf === shelf);
+  }
+
+  render () {
+    const { updateShelf } = this.props;
+
+    return(
       <div className="list-books">
         <div className="list-books-title">
           <h1>MyReads</h1>
         </div>
-        
         <div className="list-books-content">
-          <div className="list-books-content">
-            <div>
-              <BookShelf
-                name="Currently Reading"
-                books={ books }
-                // updateBookShelf={ updateBookShelf }
-              />
-              <BookShelf
-                name="Want to Read"
-                books={ books }
-                // updateBookShelf={ updateBookShelf }
-              />
-              <BookShelf
-                name="Read"
-                books={ books }
-                // updateBookShelf={ updateBookShelf }
-              />
-            </div>
+          <div>
+            <Bookshelf
+              name="Currently Reading"
+              books={ this._filterBooks('currentlyReading') }
+              updateShelf={ updateShelf }
+            />
+            <Bookshelf
+              name="Want to Read"
+              books={ this._filterBooks('wantToRead') }
+              updateShelf={ updateShelf }
+            />
+            <Bookshelf
+              name="Read"
+              books={ this._filterBooks('read') }
+              updateShelf={ updateShelf }
+            />
           </div>
-        </div>          
+        </div>
+        <div className="open-search">
+          <Link
+            to="/search"
+          >
+            Add a book
+          </Link>
+        </div>
       </div>
     );
   }
 }
 
-export default BookList
+export default BookList;
