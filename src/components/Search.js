d9e03ffed4a3c7878d268ecd55a31682486b4061
Phien Pham <phien@triip.me>
2017-09-05 11:41:57 +0700

    Update Search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,63 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
+import * as BooksAPI from './BooksAPI';
+import Book from './Book';
 
 class Search extends Component {
-    render() {
-    }
+  constructor() {
+    super();
+    this.state = {
+      query: '',
+      books: []
+    }      
+  }
+
+  updateQuery = (query) => {
+    
+  }
+
+  render() {
+    const { books } = this.state;
+    const { updateShelf } = this.props;
+
+    return(
+      <div className="search-books">
+      <div className="search-books-bar">
+        <Link
+          to="/"
+          className="close-search"
+        >
+        Close
+        </Link>
+        <div className="search-books-input-wrapper">
+          <input
+              type="text"
+              placeholder="Search by title or author"
+              onChange={ (event) => this.updateQuery(event.target.value) }
+          />
+        </div>
+      </div>
+      <div className="search-books-results">
+        <ol className="books-grid">
+          {
+            books.map((book) => (
+              <li key={ book.id }>
+                <Book
+                  id={ book.id }
+                  shelf={ book.shelf }
+                  authors={ book.authors }
+                  title={ book.title }
+                  imageLinks={ book.imageLinks }
+                  updateShelf={ updateShelf }
+              />
+            </li>
+            ))
+          }
+        </ol>
+      </div>
+    </div>
+    );    
+  }
 }
+
+export default Search
